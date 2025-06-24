c761b12d442dde726ccd4d49db70e70ee07c9443
Christopher Cook <christopher.cook@webprofusion.com>
2025-06-24 15:56:17 +0800

    Log renewal exception if present.

--- src/Certify.Core/Management/CertifyManager/CertifyManager.CertificateRequest.cs
+++ src/Certify.Core/Management/CertifyManager/CertifyManager.CertificateRequest.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -44,6 +44,7 @@ namespace Certify.Management
             catch (Exception exp)
             {
                 _tc?.TrackException(exp);
+                _serviceLog?.Error(exp, "PerformRenewalTasks: Error performing periodic renewal tasks: {exp}", exp);
                 return await Task.FromResult(false);
             }
 
